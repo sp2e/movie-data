feb923acc445fb4775bd75fcb1dff0670d682a4b
sp2e <daslen@yahoo.com>
2016-05-17 00:30:07 -0700

    mass 1st commit

--- movie-data.rb
+++ movie-data.rb
@@ -1,24 +1,88 @@
 require 'sinatra'
-require 'sinatra/activerecord'
+require 'json'
 
 get '/' do
   erb :index
 end
 
-=begin
-get 'favorites' do
-  response.header['Content-Type'] = 'application/json'
-  File.read('data.json')
+#note the relative path here (/favorites)
+#results in a routine being executed,
+#it functions similar to a "controller" in ruby-on-rails
+get '/favorites' do
+  #response.header['Content-Type'] = 'application/json'
+  #user sinatra_helper :  send_file
+  #by default, will set content_type per file passed by send_file
+  #thus, content_type 'application/json'
+  send_file 'data.json'
 end
 
-get '/favorites' do
-  file = JSON.parse(File.read('data.json'))
-  unless params[:name] && params[:oid]
-    return 'Invalid Request'
-  movie = { name: params[:name], oid: params[:oid] }
+=begin
+
+def movie_not_favorited?(file, oid)
+  fav = true
+  file.each{|movie|
+    if oid == movie["oid"]
+      fav = false
+    end
+  }
+  return fav
+end
+def add_movie(file, movie)
   file << movie
   File.write('data.json',JSON.pretty_generate(file))
-  movie.to_json
-
 end
+
 =end
+
+
+post '/favorites' do
+  #for debug, the following will output to the terminal window
+  #puts "AHHHHHHHH"
+  file = JSON.parse(File.read('data.json'))
+  # following for debug , prints to command window 
+  #puts file
+  unless params[:name] && params[:oid]
+    return 'Invalid Request'
+  end
+  not_fav = true
+  movie = { "name" => params[:name], "oid" => params[:oid] }
+  #the above syntax used so as to match JSON parse output format of strings for keys
+  #alternatively could use the following symbol format:
+  #
+  #movie = { name: params[:name], oid: params[:oid] }
+  #
+  #however, then would need the following syntax for the condition statement in the loop below
+  # 
+  #    if movie[:oid] == file_movie["oid"]
+  #
+  file.each{|file_movie|
+    #note: JSON.parse returns strings as keys to hashes
+    #whereas the movie hash was formed using symbols as keys
+    #so have to reference the hash values as follows:
+
+    if movie["oid"] == file_movie["oid"]
+      not_fav = false
+      puts not_fav
+      return 'Movie already favorited'
+    end
+  }
+
+  if not_fav == true
+    file << movie
+    File.write('data.json',JSON.pretty_generate(file))
+    return 'Movie added to favorites'
+  end
+
+  #the following doesnt seem to return anything
+  #return not_fav
+  
+#Finish this to send back info if was newly favorited movie
+#following from "get started with sinatra " bookmark
+  #content_type 'application/json'
+  #value = not_fav ?  : 'not done'
+  #{ :id => params[:id], :status => value }.to_json
+  #{ :status => not_fav }.to_json
+end
+
+
+
--- public/javascript/getElemMy.js
+++ public/javascript/getElemMy.js
@@ -10,23 +10,39 @@ form.addEventListener("submit", function(event){
   var oOutput = document.querySelector("#movieInfo");
 
   var req = new XMLHttpRequest();
-  // the "true" parameter means asynchronous.  true would be synch (depreciated now)
+  // the "true" parameter means asynchronous.  false would be synch (depreciated now)
   // this "req" variable receives the data returned by the async get request
   // and i believe the async trigger for the returned data is the "load" parameter named in the addEventListener
-  req.open("Get", movieReq, true);
+  req.open("GET", movieReq, true);
   req.addEventListener("load", function(){
     var readResp;
-    var oRead = "";
+    var oRead = "<p></p>" + "xxxxxxxxxxxxxxxxxxxx" + "</p></p>" + 
+    "xxxxxxxxxxxxxxxxxxxx" + "</p>" + "MOVIE SEARCH RESULTS" + "</p>" + "xxxxxxxxxxxxxxxxxxxx" + "</p>" ;
     if (req.status < 400){
-      //readResp = JSON.stringify(JSON.parse(req.responseText).Search);//, function(key,value){
 
-      //oOutput.innerHTML = JSON.stringify(JSON.parse(req.responseText).Search);
+      /*
+      var testText = '{"name":"John Johnson","street":"Oslo West 16","phone":"555 1234567"}';
+      post '/' do
+        File.open('data.json', 'w') do |f|
+          f.write params[testText]
+        end
+      end
+
+      get '/data.json' do |filename|
+        send_file('./#{filename}', :filename => filename, :type =>'application/octet-stream')
+      end
+      */
+      //Search is the outer most object key 
       readResp = JSON.parse(req.responseText).Search;
-      //console.log(readResp);
 
       //set up the target for the async request for finding details for a specific movie
       for (var i = readResp.length - 1; i >= 0; i--) {
-        oRead += "<p>" + "<span  id='spn" + i + "' onclick='movieDetails(this.id)'>" + readResp[i].Title + "</span><span id='detspn" + i + "'></span>" + "</p>";
+        oRead += "<p>" + "<span  id='spn" + i +
+          "' onclick='movieDetails(this.id,\"id_movie_details" + i + "\")'>" +
+          readResp[i].Title + "&nbsp;&nbsp" + "</span><span id='fav" +
+          i +"'><button type='button' onclick='addFavorite(\"" +
+          readResp[i].Title+"\",\""+ readResp[i].imdbID +
+          "\")'>Favorite</button></span>" + "<p id = 'id_movie_details" + i + "'></p> "
       };
       // so set the text for the #movieInfo location (below the form display) on the web page
       oOutput.innerHTML = oRead;
@@ -37,38 +53,29 @@ form.addEventListener("submit", function(event){
   req.send(null);
 });
 
-function movieDetails(clicked_id){
+function movieDetails(clicked_id,id_movie_details){
   var x=document.getElementById(clicked_id);
   //lert(x.innerHTML);
   var movieReq =  "http://www.omdbapi.com/?t=" + x.innerHTML + "&plot=short" ;
 
   var req = new XMLHttpRequest();
-  req.open("Get", movieReq, true);
+  req.open("GET", movieReq, true);
   req.addEventListener("load", function(){
     var readResp;
     var oRead = "";
-    var detailsSpan_id = "det" + clicked_id;
-    var detailsElem = document.getElementById(detailsSpan_id);
+
+    var movie_details = document.getElementById(id_movie_details);
     if (req.status < 400){      
       //readResp = JSON.stringify(JSON.parse(req.responseText));//, function(key,value){
       readResp = JSON.parse(req.responseText);
-      for (obj_detail in readResp) {
+      //console.log(readResp);
+      //this readResp now is an object of hashes (key:value pairs)
+      //Note in JS, the "for..in" construct operates specifically on such objects
+      //with the "obj_detail"  below getting each key in the object, one at a time.
+      for (var obj_detail in readResp) {
         oRead += "<p>" + obj_detail + " : " + readResp[obj_detail] +  "</p>";
       };
-      //oRead = readResp.Title;
-      detailsElem.innerHTML = oRead;       
-      //alert(readResp);
-      //oOutput.innerHTML = JSON.stringify(JSON.parse(req.responseText).Search);
-      /*
-      readResp = JSON.parse(req.responseText).Search;
-      console.log(readResp);
-
-      for (var i = readResp.length - 1; i >= 0; i--) {
-        oRead += "<p>" + "<span  id='spn" + i + "' onclick='movieDetails(this.id)'>" + readResp[i].Title + "</span>" + "</p>";
-      };
-
-      oOutput.innerHTML = oRead;
-      */
+      movie_details.innerHTML = oRead;       
     }else{
       alert("error");
     };
@@ -76,14 +83,62 @@ function movieDetails(clicked_id){
   req.send(null);
 }
 
-function getValue()
-  {
-  var x=document.getElementById("beside");
-  x.innerHTML  = "is this beside the click element?"
-  }
-
-function myGetElem(xx){
-  var y = document.getElementById(xx);
-  var t = "test";
-  return t;
+function getFavorites(){
+  var oOutput = document.querySelector("#listFavorites");
+  var req = new XMLHttpRequest();
+  req.open("GET", "/favorites", true);
+  req.addEventListener("load", function(){
+    var readResp;
+    var oRead =  "<p></p>" + "xxxxxxxxxxxxxxx" +"</p>" + 
+    "FAVORITE MOVIES" + "</p>" + "xxxxxxxxxxxxxxx"  + "<p></p>"
+    if (req.status < 400){      
+      readResp = JSON.parse(req.responseText);
+      //readResp is an array of objects
+      readResp.forEach(
+        function(movie){
+          oRead += movie["name"] + "</p>";
+        }
+      );
+      oOutput.innerHTML = oRead;       
+    }else{
+      alert("error");
+    };
+  });  
+  req.send(null);
 };
+
+function addFavorite(name, oid){
+
+  var url = "/favorites";
+  var params = "name=" + encodeURIComponent(name) + "&oid=" + encodeURIComponent(oid);
+  params = params.replace(/%20/g, '+');
+  var req = new XMLHttpRequest();
+
+  req.open("POST", url, true);
+
+  //Send the proper header information along with the request
+  req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  req.setRequestHeader("Content-length", params.length);
+  req.setRequestHeader("Connection", "close");
+
+  req.onreadystatechange = function() {//Call a function when the state changes.
+    //if(req.readyState == 4 && req.status == 200) {
+    if(req.readyState == 4){
+      //if(req.status == 200){
+      if(req.status < 400){
+      //alert(req.responseText);
+        if(req.responseText == "Movie added to favorites"){
+          getFavorites();
+          alert("Favorite Added: " + name );
+        } else {
+          alert("Movie was already favorited");
+        }
+      }
+    }
+  };
+  req.send(params);
+  //console.log(params);
+  return false;
+};
+
+
